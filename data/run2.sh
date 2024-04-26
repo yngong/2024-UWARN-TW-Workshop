fce796348a10c3f32ab7dd0867896affcdd396f2
yngong <yngong0519@gmail.com>
2024-04-26 13:22:12 +0800

    update day2

---
+++ data/run2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+val=`expr 3 + 1`
+echo "Sum : $val"
+
+## The above script will generate the following result
+## Sum : 2
---
+++ data/run4.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+# Retrieve file names and print out
+
+### get args
+while getopts i:o: option
+do
+case "${option}"
+in
+i) i=${OPTARG};;
+o) o=${OPTARG};;
+esac
+done
+
+### check args
+if [ "$i" == "" -o "$o" == "" ]
+then
+	echo "Input Error, please make sure"
+	echo "-i as input file name"
+	echo "-o as output file name"
+	echo "Usage: sh printInputOutput.sh -i input.fas -o output.fas"
+	exit
+fi
+
+echo "input file name: "$i
+echo "output file name: "$o
+Read a fasta file and generate a CSV file
+! cp /home/public/ngs/script/generate_csv.sh ./
+! bash generate_csv.sh -i input.fas -o output.fas
+#!/bin/bash
+# This script takes fasta data to generate CSV file.
+
+### get args
+while getopts i:o: option
+do
+case "${option}"
+in
+i) i=${OPTARG};;
+o) o=${OPTARG};;
+esac
+done
+
+### check args
+if [ "$i" == "" -o "$o" == "" ]
+then
+	echo "Input Error, please make sure"
+	echo "-i as input file name"
+	echo "-o as output file name"
+	echo "Usage: bash generate_csv.sh -i input.fas -o output.fas"
+	exit
+fi
+
+echo "input file name: "$i
+echo "output file name: "$o
+
+# step 1. create CSV file
+echo "virus,location,strain,year,accession_number,collection_date,area" > $o
+# step 2. retrieve meta data and stream editor
+grep ">" $i | sed 's/>//g' | sed 's/\// | /g' | sed 's/ | /,/g' >> $o
+
